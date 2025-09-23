888a5a84389640b647b7b53e22f70a2bb59b5d82
tim-ming <tim.ming@icloud.com>
2025-09-24 03:49:38 +0800

    wip: fix missing aboutsection import

--- src/sections/index.ts
+++ src/sections/index.ts
@@ -1,6 +1,5 @@
 export { default as IntroSection } from './IntroSection';
 export { default as NowSection } from './NowSection';
-export { default as AboutSection } from './AboutSection';
 export { default as ProjectsSection } from './ProjectsSection';
 export { default as EducationSection } from './EducationSection';
 export { default as ExperienceSection } from './ExperienceSection';
